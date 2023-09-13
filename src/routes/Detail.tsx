3c93238eaeb17739a21cfc8a42414ed40c9588d8
UHquokka <bananamayong@gmail.com>
2023-09-13 21:27:41 +0900

    📝: 안쓰는 React 코드 정리

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ThemeContext, ThemeContextType } from '../App.tsx'
 import Comics from '../components/Comics';
