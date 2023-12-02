e49af310b1400841eeba0b71d1c382dca60b8ec6
UHquokka <bananamayong@gmail.com>
2023-12-02 23:58:54 +0900

    fix: 404 page 수정 및 vercel.json 추가

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -4,7 +4,7 @@ export default function ErrorPage() {
     return (
         <div>
             <h1>Oops! </h1>
-            <span>Sorry, an unexpected error ha occured</span>
+            <span>Sorry, an unexpected error has occured</span>
 
             <Link to={'/'}>Back to Home</Link>
         </div>
