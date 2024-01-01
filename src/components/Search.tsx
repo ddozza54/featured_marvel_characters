1b9dfecd3c0f04a28d660a0a157c41cfeba74d2a
UHquokka <bananamayong@gmail.com>
2024-01-01 23:00:24 +0900

    feat: Search 컴포넌트 추가

---
+++ src/components/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Search() {
+    return (
+        <div>
+            <span>🔍</span>
+            <input placeholder='찾고 싶은 캐릭터가 있나요?' />
+        </div>
+    );
+}
+
